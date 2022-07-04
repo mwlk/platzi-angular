f6952d0bf637ce7f0b8a88ac7ce58dec6b5a43e7
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-07-03 21:42:29 -0300

    add store service

---
+++ src/app/services/store.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../shared/models/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StoreService {
+  private myShoppingCart: Product[] = [];
+  constructor() {}
+
+  addProduct(p: Product) {
+    this.myShoppingCart.push(p);
+  }
+
+  getShoppingCart(){
+    return this.myShoppingCart;
+  }
+
+  getTotal() {
+    return this.myShoppingCart.reduce((sum, item) => sum + item.precio, 0);
+  }
+}
