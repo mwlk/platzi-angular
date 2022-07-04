6088e0d22e2ec0dd1272e09421194ceec2286aaf
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-07-04 01:17:00 -0300

    add rxjs in shopping cart

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../shared/models/product';
 
+import { BehaviorSubject } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
 export class StoreService {
   private myShoppingCart: Product[] = [];
+
+  private myCart = new BehaviorSubject<Product[]>([]);
+
+  myCart$ = this.myCart.asObservable();
+
   constructor() {}
 
   addProduct(p: Product) {
     this.myShoppingCart.push(p);
+    this.myCart.next(this.myShoppingCart);
   }
 
-  getShoppingCart(){
+  getShoppingCart() {
     return this.myShoppingCart;
   }
 
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   showMenu: boolean = false;
+  counter: number = 0;
 
-  constructor() {}
+  constructor(private _storeSvc: StoreService) {}
+  ngOnInit(): void {
+    this._storeSvc.myCart$.subscribe((productList) => {
+      this.counter = productList.length;
+    });
+  }
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
