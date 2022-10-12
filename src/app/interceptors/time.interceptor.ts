a3085bf832928ad349df25d6b1bece5593038396
mwlk <mirkoivowlk@gmail.com>
2022-10-11 23:58:42 -0300

    add interceptor context

--- src/app/interceptors/time.interceptor.ts
+++ src/app/interceptors/time.interceptor.ts
@@ -4,8 +4,16 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
+  HttpContext,
+  HttpContextToken,
 } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
+
+const CHECK_TIME = new HttpContextToken<boolean>(() => false);
+
+export function checkTime() {
+  return new HttpContext().set(CHECK_TIME, true);
+}
 
 @Injectable()
 export class TimeInterceptor implements HttpInterceptor {
@@ -15,13 +23,17 @@ export class TimeInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    const start = performance.now();
+    if (request.context.get(CHECK_TIME)) {
+      const start = performance.now();
 
-    return next.handle(request).pipe(
-      tap((res) => {
-        const time = performance.now() - start + 'ms';
-        console.info(`${request.url} -> ${time}`);
-      })
-    );
+      return next.handle(request).pipe(
+        tap((res) => {
+          const time = performance.now() - start + 'ms';
+          console.info(`${request.url} -> ${time}`);
+        })
+      );
+    }
+
+    return next.handle(request);
   }
 }
