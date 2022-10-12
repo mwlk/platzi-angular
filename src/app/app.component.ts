442c2e1036a3fd51ecf97d162627cbf0eb7d2d49
mwlk <mirkoivowlk@gmail.com>
2022-10-11 23:59:14 -0300

    add file system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { switchMap } from 'rxjs';
-import { AuthService } from './services/auth.service';
+import { Component } from '@angular/core';
+import { FileService } from './services/file.service';
 import { UserService } from './services/user.service';
 import { User } from './shared/models/user';
 
@@ -9,17 +8,18 @@ import { User } from './shared/models/user';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Platzi';
   profile!: User;
+  imgRta = '';
 
   /**
    *
    */
-  constructor(private _authSvc: AuthService, private _userSvc: UserService) {}
-  ngOnInit(): void {
-    this.login();
-  }
+  constructor(private _userSvc: UserService, private _fileSvc: FileService) {}
+  // ngOnInit(): void {
+  //   this.login();
+  // }
 
   createUser() {
     this._userSvc.create({
@@ -29,18 +29,37 @@ export class AppComponent implements OnInit {
     });
   }
 
-  login() {
-    this._authSvc
-      .login('mirko.wlk@dicsys.com', '123456')
-      .pipe(
-        switchMap((token) => {
-          console.log(token.access_token);
-          return this._authSvc.profile(token.access_token);
-        })
+  // login() {
+  //   this._authSvc
+  //     .login('mirko.wlk@dicsys.com', '123456')
+  //     .pipe(
+  //       switchMap((token) => {
+  //         console.log(token.access_token);
+  //         return this._authSvc.profile(token.access_token);
+  //       })
+  //     )
+  //     .subscribe((data) => {
+  //       this.profile = data;
+  //       console.log(this.profile);
+  //     });
+
+  downloadDoc() {
+    this._fileSvc
+      .getFile(
+        'test',
+        'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf',
+        'application/pdf'
       )
-      .subscribe((data) => {
-        this.profile = data;
-        console.log(this.profile);
+      .subscribe();
+  }
+
+  uploadDoc(event: Event) {
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0);
+    if (file) {
+      this._fileSvc.uploadFile(file).subscribe((res) => {
+        this.imgRta = res.location;
       });
+    }
   }
 }
